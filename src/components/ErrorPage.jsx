e57aabcbf6e54932640746f237ed4e1ca077f998
PPS-Fredkamm <fred.kamm@proactiveusa.com>
2025-05-29 15:39:40 -0400

    updating navbar style. giving the error page a basic update

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Button, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>ErrorPage</div>
-  )
+    <Container className="error-page text-center d-flex flex-column justify-content-center align-items-center">
+      <h1 className="display-4">Oops!</h1>
+      <p className="lead">The page you're looking for doesn't exist.</p>
+      <Button variant="primary" onClick={() => navigate('/')}>Return to Home</Button>
+    </Container>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 
 import ProfilePlaceholder from '../../assets/Images/profilePlaceholder.jpg';
 
@@ -22,54 +23,79 @@ function CustomNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary px-3">
       <Container fluid>
-        <Navbar.Brand href="/">Alumbiz</Navbar.Brand>
-        <Nav className="me-auto">
-          <NavDropdown title="Menu" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#">Action</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
+        <div className="d-flex w-100 justify-content-between align-items-center">
+          {/* Left-aligned brand */}
+          <Navbar.Brand href="/">Alumbiz</Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            {!isAuthenticated ? (
-              <>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/signup">Signup</Nav.Link>
-              </>
-            ) : (
-              <NavDropdown
-                align="end"
-                title={
-                  <Image
-                    src={user.profileImg}
-                    roundedCircle
-                    width={40}
-                    height={40}
-                    alt="Profile"
-                    className="profile-icon"
-                  />
-                }
-                id="profile-dropdown"
-                className="profile-dropdown"
-              >
-                <div className="profile-card">
-                  <Image
-                    src={user.profileImg}
-                    className="profile-card-img"
-                    alt="Profile"
-                  />
-                  <div className="profile-card-info">
-                    <strong>{user.name}</strong>
-                    <div className="text-muted">{user.email}</div>
-                  </div>
-                </div>
-                <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
-                <NavDropdown.Item onClick={logout}>Sign Out</NavDropdown.Item>
-              </NavDropdown>
-            )}
+          {/* Center-aligned menu */}
+          <Nav className="nav-center">
+            <Nav.Link href="/link">Why Donate Stock</Nav.Link>
+            <Nav.Link href="/link">For Donors</Nav.Link>
+            <Nav.Link href="/link">For Universities</Nav.Link>
+            <Nav.Link href="/link">Partners</Nav.Link>
+            <Nav.Link href="/link">Resources</Nav.Link>
+            <Nav.Link href="/link">Partners</Nav.Link>
+            {/* <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Action</NavDropdown.Item>
+            </NavDropdown> */}
           </Nav>
-        </Navbar.Collapse>
+
+          {/* Right-aligned user profile */}
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                {!isAuthenticated ? (
+                  <>
+                    <Button
+                      href="/donate"
+                      variant="primary"
+                      className="donate-button"
+                    >
+                      Donate Shares
+                    </Button>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/signup">Signup</Nav.Link>
+                  </>
+                ) : (
+                  <NavDropdown
+                    align="end"
+                    title={
+                      <Image
+                        src={user.profileImg}
+                        roundedCircle
+                        width={40}
+                        height={40}
+                        alt="Profile"
+                        className="profile-icon"
+                      />
+                    }
+                    id="profile-dropdown"
+                    className="profile-dropdown"
+                  >
+                    <div className="profile-card">
+                      <Image
+                        src={user.profileImg}
+                        className="profile-card-img"
+                        alt="Profile"
+                      />
+                      <div className="profile-card-info">
+                        <strong>{user.name}</strong>
+                        <div className="text-muted">{user.email}</div>
+                      </div>
+                    </div>
+                    <NavDropdown.Item href="/profile">
+                      My Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logout}>
+                      Sign Out
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </div>
       </Container>
     </Navbar>
   );
