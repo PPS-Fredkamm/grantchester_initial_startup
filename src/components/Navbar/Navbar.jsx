cd4f2d580baa6860629ebf51524b3107d7c57702
PPS-Fredkamm <fred.kamm@proactiveusa.com>
2025-05-29 21:15:34 -0400

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,40 +13,7 @@ import ProfilePlaceholder from '../../assets/Images/profilePlaceholder.jpg';
 
 import { useUser } from '../../Data/UserContext';
 import './Navbar.css';
-import React, { useState } from 'react';
 
-function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-
-  return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-brand">
-        Alumbiz
-      </Link>
-
-      
-      <div className="navbar-links">
-        <Link to="/login" className="btn-signin">Sign in</Link>
-        <Link to="/signup" className="btn-signup">Sign up</Link>
-          <div className="hamburger">☰</div>
-      </div>
-
-      {/* ✅ Move hamburger here */}
-  <div className="dropdown-container">
-    <div className="hamburger" onClick={toggleMenu}>
-      TEST
-    </div>
-    {menuOpen && (
-      <div className="dropdown-menu">
-        <Link to="/profile" onClick={() => setMenuOpen(false)}>Profile</Link>
-        <Link to="/settings" onClick={() => setMenuOpen(false)}>Settings</Link>
-        <Link to="/logout" onClick={() => setMenuOpen(false)}>Log Out</Link>
-      </div>
-    )}
-  </div>
-</nav>
 function CustomNavbar() {
   const { isAuthenticated, logout } = useUser();
 
